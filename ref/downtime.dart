07e616434eba9790142d34285b7f46234fd0c77c
vaibhavjain2024 <vaibhav.jain05@nagarro.com>
2025-04-23 15:51:09 +0530

    update downtime api status

--- ref/downtime.dart
+++ ref/downtime.dart
@@ -4,39 +4,39 @@
             "routeName" : "pressShop/downtime/filters",
             "lambdaName": "msil-iot-psm-get-downtime-filters",
             "methodType":["GET"]
-        },
+        } -> Done,
         {
             "httpApiGatewayName": "msil-iot-pressshop-apis",
             "httpAuthorizerName":"pressShopCognitoAuthorizer",
             "routeName" : "pressShop/downtime",
             "lambdaName": "msil-iot-psm-get-downtime",
             "methodType":["GET"]
-        },
+        } -> Done,
         {
             "httpApiGatewayName": "msil-iot-pressshop-apis",
             "httpAuthorizerName":"pressShopCognitoAuthorizer",
             "routeName" : "pressShop/downtime/report",
             "lambdaName": "msil-iot-psm-get-downtime-report",
             "methodType":["GET"]
-        },
+        } -> Done,
         {
             "httpApiGatewayName": "msil-iot-pressshop-apis",
             "httpAuthorizerName":"pressShopCognitoAuthorizer",
             "routeName" : "pressShop/downtime/totalduration",
             "lambdaName": "msil-iot-psm-get-total-downtime",
             "methodType":["GET"]
-        },
+        } -> Done,
         {
             "httpApiGatewayName": "msil-iot-pressshop-apis",
             "httpAuthorizerName":"pressShopCognitoAuthorizer",
             "routeName" : "pressShop/downtime/remark/list",
             "lambdaName": "msil-iot-psm-downtime-remark-list",
             "methodType":["GET"]
-        },
+        } -> Done,
         {
             "httpApiGatewayName": "msil-iot-pressshop-apis",
             "httpAuthorizerName":"pressShopCognitoAuthorizer",
             "routeName" : "pressShop/downtime/remark/update",
             "lambdaName": "msil-iot-psm-downtime-remark-update",
             "methodType":["PUT"]
-        }
+        } -> Done
