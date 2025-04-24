90cdc65eded60b77606201e3f71ee67c1697131a
vaibhavjain2024 <vaibhav.jain05@nagarro.com>
2025-04-25 01:09:31 +0530

    plan schema

---
+++ app/schema/planSchema.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class getPlan(BaseModel):
+    shop_id: int
+    page_no: int = 1
+    page_size: int = 10
+    model_list: Optional[str] = None
+    machine_list: Optional[str] = None
+    part_name_list: Optional[str] = None
+    pe_code_list: Optional[str] = None
+    production_date_list: Optional[str] = None
+    shift: Optional[str] = None
+    priority: Optional[str] = None
+    status: Optional[str] = None
+    sort_priority: Optional[str] = None
+    
+
+class getPlanReport(BaseModel):
+    shop_id: int
+    model_list: Optional[str] = None
+    machine_list: Optional[str] = None
+    part_name_list: Optional[str] = None
+    pe_code_list: Optional[str] = None
+    production_date_list: Optional[str] = None
+    shift: Optional[str] = None
+    priority: Optional[str] = None
+    status: Optional[str] = None
+    sort_priority: Optional[str] = None
+
+
+class getPlanFilter(BaseModel):
+    shop_id: int
+
+
+class planDownload(BaseModel):
+    shop_id: int = Field(..., description="The ID of the shop")
+    shop_name: Optional[str] = Field(None, description="The name of the shop")
+    date: Optional[str] = Field(None, regex=r"^\d{4}-\d{2}-\d{2}$", description="The date in YYYY-MM-DD format")
+
+
+class planUpload(BaseModel):
+    shop_id: int = Field(..., description="The ID of the shop")
+    shop_name: Optional[str] = Field(None, description="The name of the shop")
